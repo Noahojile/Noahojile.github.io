dc0e641985d1afe8e2a38bc03daa2bf68cb58a43
Noah Ojile <104537320+Noahojile@users.noreply.github.com>
2024-09-15 09:53:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify, render_template
-from tensorflow.keras.models import load_model
+from tensorflow.lite.python.interpreter import Interpreter
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 
 app = Flask(__name__)
 
-# Load your model
-model = load_model('path_to_model/final_model.keras')
+# Load TensorFlow Lite model
+interpreter = Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 # Define class labels (adjust according to your dataset)
-class_labels = ['Class1', 'Class2', 'Class3']
+class_labels = ['Bean_Rust', 'Angular_Leaf_Spot', 'Healthy']
 
 # Prediction endpoint
 @app.route('/predict', methods=['POST'])
@@ -29,10 +34,16 @@ def predict():
     # Load and preprocess the image
     img = image.load_img(filepath, target_size=(150, 150))
     img_array = image.img_to_array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=0).astype(np.float32)
 
-    # Make prediction
-    predictions = model.predict(img_array)
+    # Set the input tensor
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+
+    # Run inference
+    interpreter.invoke()
+
+    # Get prediction result
+    predictions = interpreter.get_tensor(output_details[0]['index'])
     predicted_class = class_labels[np.argmax(predictions)]
 
     return jsonify({'class': predicted_class})
@@ -44,4 +55,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-  
